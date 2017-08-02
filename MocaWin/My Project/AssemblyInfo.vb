5a322fd31dc032dab254bfc9e9cba94a7cd8458c
MiYABiS <miyabis@live.jp>
2017-08-03 03:08:28 +0900

    プロジェクト構成とバージョン付けの変更

--- MocaWin/My Project/AssemblyInfo.vb
+++ MocaWin/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' アセンブリ属性の値を確認します。
 
-<Assembly: AssemblyDescription("MocaWin")>
-<Assembly: AssemblyCompany("MiYABiS")> 
-<Assembly: AssemblyProduct("MocaWin")> 
-<Assembly: AssemblyCopyright("Copyright © 2011 MiYABiS All Rights Reserved.")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyDescription("Moca.NET Windows Form")>
+<Assembly: AssemblyCompany("MiYABiS")>
+<Assembly: AssemblyProduct("Moca.NET Framework")>
+<Assembly: AssemblyCopyright("© MiYABiS All Rights Reserved.")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(True)> 
 
@@ -33,51 +33,33 @@ Imports System.Runtime.InteropServices
 ' プログラム要素が CLS (Common Language Specification) に準拠しているかどうかを示します
 <Assembly: System.CLSCompliant(True)>
 
+
+<Assembly: AssemblyVersion("5.0.0")>
+<Assembly: AssemblyFileVersion("3.0.0")>
+<Assembly: AssemblyInformationalVersion("3.0.0")>
+
+
 #If net20 Then
-<Assembly: AssemblyVersion("2.0.0")>
-<Assembly: AssemblyFileVersion("2.0.0")>
-<Assembly: AssemblyTitle("Moca.NET Win .NET 2.0")>
-<Assembly: AssemblyInformationalVersion("2.0.0 .NET 2.0")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 2.0")>
 #End If
 #If net35 Then
-<Assembly: AssemblyVersion("3.5.0")> 
-<Assembly: AssemblyFileVersion("3.5.0")> 
-<Assembly: AssemblyTitle("Moca.NET Win .NET 3.5")>
-<Assembly: AssemblyInformationalVersion("3.5.0 .NET 3.5")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 3.5")>
 #End If
 #If net40 Then
-<Assembly: AssemblyVersion("4.0.0")>
-<Assembly: AssemblyFileVersion("4.0.0")>
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.0")>
-<Assembly: AssemblyInformationalVersion("4.0.0 .NET 4.0")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.0")>
 #End If
 #If net45 Then
-<Assembly: AssemblyVersion("4.5.0")> 
-<Assembly: AssemblyFileVersion("4.5.0")> 
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.5")>
-<Assembly: AssemblyInformationalVersion("4.5.0 .NET 4.5")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.5")>
 #End If
 #If net452 Then
-<Assembly: AssemblyVersion("4.5.2")> 
-<Assembly: AssemblyFileVersion("4.5.2")> 
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.5.2")>
-<Assembly: AssemblyInformationalVersion("4.5.2 .NET 4.5.2")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.5.2")>
 #End If
 #If net46 Then
-<Assembly: AssemblyVersion("4.6.0")>
-<Assembly: AssemblyFileVersion("4.6.0")>
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.6")>
-<Assembly: AssemblyInformationalVersion("4.6.0 .NET 4.6")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.6")>
 #End If
 #If net462 Then
-<Assembly: AssemblyVersion("4.6.2")>
-<Assembly: AssemblyFileVersion("4.6.2")>
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.6.2")>
-<Assembly: AssemblyInformationalVersion("4.6.2 .NET 4.6.2")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.6.2")>
 #End If
 #If net47 Then
-<Assembly: AssemblyVersion("4.7.0")>
-<Assembly: AssemblyFileVersion("4.7.0")>
-<Assembly: AssemblyTitle("Moca.NET Win .NET 4.7")>
-<Assembly: AssemblyInformationalVersion("4.7.0 .NET 4.7")>
+<Assembly: AssemblyTitle("Moca.NET Windows Form .NET 4.7")>
 #End If
